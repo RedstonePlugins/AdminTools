138a9f8b293070b37d11e57f9a120fee07d132e3
educatalan02 <educatalan02@gmail.com>
2022-04-22 17:25:17 +0200

    Added example command

---
+++ Commands/ExampleCommand.cs
@@ -0,0 +1,47 @@
+﻿using RedstonePlugins.AdminTools.Helpers;
+using Rocket.API;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedstonePlugins.AdminTools.Commands
+{
+    public class ExampleCommand : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+
+        public string Name => "example";
+
+        public string Help => "This is an example command.";
+
+        public string Syntax => "example";
+
+        public List<string> Aliases => new List<string>() { "ex" };
+
+        public List<string> Permissions => new List<string>() { "admintools.commands.example" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            // Do something
+
+            var playerCaller = (UnturnedPlayer)caller;
+
+            if (!PlayerHelper.isPlayerOnline(playerCaller.CSteamID))
+                return;
+
+
+            // Do something with playerCaller
+
+
+
+            /* Translations usage */
+
+            TranslationHelper.SendMessageTranslation(playerCaller.CSteamID, "command_example_success", playerCaller.CharacterName);
+
+            
+        }
+    }
+}
