a050328523bf4a8893485fa8b704956e3b061a37
Edu <educatalan02@gmail.com>
2022-04-22 19:28:55 +0200

    Added getPlayerName api

--- Helpers/PlayerHelper.cs
+++ Helpers/PlayerHelper.cs
@@ -18,7 +18,29 @@ namespace RedstonePlugins.AdminTools.Helpers
             return Provider.clients.Select(x => x.playerID.steamID).Contains(SteamId);
         }
 
-        
+        /* Get Player Name */
+
+        public static string getPlayerName(CSteamID SteamId)
+        {
+            return PlayerTool.getPlayer(SteamId).channel.owner.playerID.playerName;
+        }
+        public static string getPlayerName(SteamPlayer SteamId)
+        {
+            return SteamId.playerID.playerName;
+        }
+        public static string getPlayerName(ulong SteamId)
+        {
+            PlayerTool.tryGetSteamID(SteamId.ToString(), out CSteamID steamId);
+            return getPlayerName(SteamId);
+        }
+
+        public static string getPlayerName(string SteamId)
+        {
+            PlayerTool.tryGetSteamID(SteamId, out CSteamID steamId);
+            return getPlayerName(steamId);
+        }
+
+
         /* Holding Item */
         public static bool isPlayerHoldingItem(UnturnedPlayer player)
         {
