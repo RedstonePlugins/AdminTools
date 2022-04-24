0cad404a187cdcd64c78df2ad6558d7256fc8a61
educatalan02 <educatalan02@gmail.com>
2022-04-24 06:26:59 +0200

    Added break command

---
+++ Commands/BreakCommand.cs
@@ -0,0 +1,44 @@
+﻿using RedstonePlugins.AdminTools.Helpers;
+using Rocket.API;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedstonePlugins.AdminTools.Commands
+{
+    public class BreakCommand : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+        public string Name => "break";
+
+        public string Help => "";
+
+        public string Syntax => "Syntax: /break";
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>() { "admintools.commands.break" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            var playerCaller = (UnturnedPlayer)caller;
+
+            if (!PlayerHelper.isPlayerOnline(playerCaller.CSteamID))
+                return;
+
+
+            var barricade = RaycastHelper.getBarricade(playerCaller.Player);
+
+            if (barricade != null)
+                BarricadeManager.damage(barricade.transform, barricade.hp.hp, 1, false, playerCaller.CSteamID, EDamageOrigin.Unknown);
+
+            TranslationHelper.SendMessageTranslation(playerCaller.CSteamID, "command_break_success");
+                
+
+        }
+    }
+}
